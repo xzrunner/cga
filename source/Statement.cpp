af26a6146c40195af4daf7d8049291247df74b3c
zz <xzrunner@gmail.com>
2020-01-10 07:16:55 +0800

    [FIXED] TK_LBRACE and TK_RBRACE to expr

--- source/Statement.cpp
+++ source/Statement.cpp
@@ -6,58 +6,34 @@
 namespace cgac
 {
 
-static int FIRST_Statement[] = { FIRST_STATEMENT, 0};
-
 StmtNodePtr StatementParser::ParseStatement(Parser& parser)
 {
-    switch (parser.CurrTokenType())
-    {
-    case TK_LBRACE:
-        return ParseCompoundStatement(parser);
-
-    default:
-        return ParseExpressionStatement(parser);
-    }
+    return ParseCompoundStatement(parser);
 }
 
 /**
  *  compound-statement:
- *		{ [statement-list] }
- *  duplicate-statement:
- *		{ [statement-list] }*
+ *		[statement-list]
  *  statement-list:
  *		statement
  *		statement-list statement
  */
 StmtNodePtr StatementParser::ParseCompoundStatement(Parser& parser)
 {
-//	Level++;
-
     auto comp_stmt = std::make_shared<CompoundStmtNode>(parser.GetTokenizer(), NK_CompoundStatement);
 
-	parser.NextToken();
     NodePtr* tail = &comp_stmt->stmts;
-	while (parser.CurrTokenType() != TK_RBRACE && parser.CurrTokenType() != TK_END)
-	{
-		*tail = ParseStatement(parser);
-		tail = &(*tail)->next;
-        if (parser.CurrTokenType() == TK_RBRACE) {
-            break;
-        }
-		parser.SkipTo(FIRST_Statement);
-	}
-	parser.Expect(TK_RBRACE);
+    while (parser.CurrTokenType() != TK_END)
+    {
+        *tail = ParseExpressionStatement(parser);
+        tail = &(*tail)->next;
+    }
     parser.NextToken();
 
-    if (parser.CurrTokenType() == TK_MUL) {
-        comp_stmt->duplicate = true;
-        parser.NextToken();
-    }    
-
     ASTHelper::PostCheckTypedef();
-//	Level--;
+    //	Level--;
 
-	return comp_stmt;
+    return comp_stmt;
 }
 
 /**
